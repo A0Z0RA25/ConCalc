83f14ef6aa6a16e6e3229311493d3ee0d15cfb47
ao_zora_25 <98691018+A0Z0RA25@users.noreply.github.com>
2024-05-03 19:54:46 +0800

    Add files via upload

---
+++ ellipse GF/ellipseGF.js
@@ -0,0 +1,36 @@
+const numeratorx = document.querySelector(".numeratorx");
+const denominatorx = document.querySelector(".denominatorx");
+const numeratory = document.querySelector(".numeratory");
+const denominatory = document.querySelector(".denominatory");
+const result = document.querySelector(".parenthesis");
+function transform(){
+ 
+    const x2 = parseFloat(document.querySelector(".x2").value);
+    const y2 = parseFloat(document.querySelector(".y2").value);
+    const coeffX = parseFloat(document.querySelector(".coeffX").value);
+    const coeffY = parseFloat(document.querySelector(".coeffY").value);
+    const constant = parseFloat(document.querySelector(".constant").value);
+
+
+    const centerX = coeffX / x2;
+    const centerY = coeffY / y2;
+    const constX = Math.pow(centerX / 2, 2);
+    const constY = Math.pow(centerY / 2, 2);
+    const radius = ((constX * x2)  + (constY * y2)) + (constant / -1);
+console.log(radius)
+console.log(constX) 
+console.log(constY)
+    const numeX = centerX / 2;
+    const numeY = centerY / 2;
+    const denoX = radius / x2;
+    const denoY = radius / y2;
+
+
+    numeratorx.innerHTML = `x+${numeX}`;
+    denominatorx.innerHTML = denoX;
+
+    numeratory.innerHTML = `x+${numeY}`;
+    denominatory.innerHTML = denoY;
+
+    result.style.display = "flex";
+}
---
+++ script.js
@@ -0,0 +1,71 @@
+const intro = document.querySelector(".introduction");
+const about = document.querySelector(".about-page");
+const circleList = document.querySelector(".transformCircle");
+const parabolaList = document.querySelector(".transformParabola");
+const ellipseList = document.querySelector(".transformEllipse");
+const hyperbolaList = document.querySelector(".transformHyperbola");
+
+const sideNav = document.querySelector(".side-nav");
+const hamburgerBar = document.querySelector(".side-nav-btn");
+function sideNavBtn(){
+    sideNav.classList.toggle("open"); 
+
+    if(sideNav.classList.contains("open")) {
+        sideNav.style.transform = "translateX(0)";
+        hamburgerBar.textContent = "X"; 
+    } else {
+        sideNav.style.transform = "translateX(-100%)";
+        hamburgerBar.textContent = "☰"; 
+    }
+}
+
+// SHOW BTN
+function introduction() {
+    if (intro.style.display === "none") {
+        intro.style.display = "block";
+        about.style.display = "none";
+    } else {
+        intro.style.display = "block";
+    }
+}
+ 
+function aboutUs() {
+    if (about.style.display === "none") {
+        about.style.display = "block";
+        intro.style.display = "none";
+    } else {
+        about.style.display = "block";
+    }
+}
+
+function circle() {
+    if (circleList.style.display === "none") {
+        circleList.style.display = "block";
+    } else {
+        circleList.style.display = "none";
+    }
+}
+
+function ellipse() {
+    if (ellipseList.style.display === "none") {
+        ellipseList.style.display = "block";
+    } else {
+        ellipseList.style.display = "none";
+    }
+}
+
+function parabola() {
+    if (parabolaList.style.display === "none") {
+        parabolaList.style.display = "block";
+    } else {
+        parabolaList.style.display = "none";
+    }
+}
+
+function hyperbola() {
+    if (hyperbolaList.style.display === "none") {
+        hyperbolaList.style.display = "block";
+    } else {
+        hyperbolaList.style.display = "none";
+    }
+}
